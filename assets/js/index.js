2828e86477e80fa95c666b76ed73dcbc66e32c58
Mohammed Ghanam <127614412+mido-ghanam@users.noreply.github.com>
2025-01-17 14:38:27 +0200

    Add files via upload

--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const logo = document.querySelector('.acadmy_float_logo');
-    logo.addEventListener('click', function() {
-        logo.classList.add('clicked');
-    });
-});
